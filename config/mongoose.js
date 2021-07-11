bd2a35592020a90ba48007ce8a699be154f77910
Ujjwal Suman <ujjwalsuman007@gmail.com>
2021-07-11 17:10:43 +0530

    eslint conf complete

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://node-api:ozdqw5qu14putjX9@node-api.kzqij.mongodb.net/node?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect('mongodb+srv://node-api:ozdqw5qu14putjX9@node-api.kzqij.mongodb.net/node?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
+// eslint-disable-next-line no-console
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('database connection open')
+db.once('open', () => {
+  // eslint-disable-next-line no-console
+  console.log('database connection open');
 });
 
 module.exports = db;
-
