5da85c74e07c763532670e4e660cef3b51058f38
Ujjwal Suman <ujjwalsuman007@gmail.com>
2021-07-11 19:00:41 +0530

    eslint conf complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express =  require('express');
+const express = require('express');
+
 const app = express();
 const port = 7000;
 require('./config/mongoose');
@@ -6,13 +7,15 @@ require('./config/mongoose');
 app.use(express.urlencoded());
 app.use(express.json());
 
-//use express router
+// use express router
 app.use('/', require('./router'));
 
 app.listen(port, (err) => {
   if (err) {
+    // eslint-disable-next-line no-console
     console.log(`error to fire up the server: ${err}`);
     return;
-  } 
+  }
+  // eslint-disable-next-line no-console
   console.log(`server is running on port : ${port}`);
 });
