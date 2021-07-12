64a5f00540bf17388dadd0b5bd613ea0b0b25c02
Ujjwal Suman <ujjwalsuman007@gmail.com>
2021-07-12 20:51:49 +0530

    updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/node-api', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://node-api:ozdqw5qu14putjX9@node-api.kzqij.mongodb.net/node?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 // eslint-disable-next-line no-console
 db.on('error', console.error.bind(console, 'connection error:'));
